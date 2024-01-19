3e8b2928c36aa922171cb665cee8d70f7372a0f9
Pushpanjali chaurasiya <122771267+Pushpanjali2001chaurasiya@users.noreply.github.com>
2024-01-20 00:47:22 +0530

    Update settings.py

--- RO_SYS/settings.py
+++ RO_SYS/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
